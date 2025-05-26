61d50e342edd6848949f843a231a80afdd2073e3
Md-SAMI-7 <isss.abdussami@gmail.com>
2025-05-26 20:01:35 +0530

    Update gemini_backend.py

--- gemini_backend.py
+++ gemini_backend.py
@@ -2,14 +2,17 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import google.generativeai as genai
 import os
+from dotnev import load_dotenv
 import fitz  #For PDF
 import docx  #For DOCX
 import openpyxl  #For Excel
 
+load_dotenv()
+
 app = Flask(__name__)
 CORS(app)
 
-genai.configure(api_key="AIzaSyBmXJCWS-XUuooAWGJwVosfSo-5ufi58Go")
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-2.0-flash")
 
 def extract_text(file_storage):
